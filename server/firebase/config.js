65ae67da954e9562a800bfc68bcd1a63ffce0ade
Christian <ccastro715@gmail.com>
2019-02-25 17:17:14 -0800

    added config.js

---
+++ server/firebase/config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBactDMuN5sBiFCGs2_-KipbDxCa-73I-E",
+    authDomain: "se-165.firebaseapp.com",
+    databaseURL: "https://se-165.firebaseio.com",
+    projectId: "se-165",
+    storageBucket: "se-165.appspot.com",
+    messagingSenderId: "555597585343"
+  };
+  
+  export default config;
